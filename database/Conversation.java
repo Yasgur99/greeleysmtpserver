51faac3d595243fe38fbc7fc3bf1d802431e809b
J <knitmeascharf@users.noreply.github.com>
2017-02-07 08:32:05 -0500

    serializer

--- database/Conversation.java
+++ database/Conversation.java
@@ -1,38 +1,61 @@
 import java.utils.*;
+import java.io.*;
 
 public class Conversation {
 	/*
 	*The record of a chain of emails to a certain person with a certain subject line.
 	*Basically just an array
 	*/
-	private String senderAddress;
-	private String senderName;
-	private String recipientAddress;
-	private String recipientName;
+	private String[] senderAddress;
+	private String[] senderName;
+	private String[] recipientAddress;
+	private String[] recipientName;
+	private String[] fileName;
 	ArrayList<Email> emailCollection = new ArrayList<Email>();
 
-	public Conversation(String senderAddress, String senderName, String recipientAddress, String recipientName) {
+	public Conversation(String[] fileName, String[] senderAddress, String[] senderName, String[] recipientAddress, String[] recipientName) {
 		this.senderAddress = senderAddress;
 		this.senderName = senderName;
 		this.recipientAddress = recipientAddress;
 		this.recpientName = recpientName;
+		this.fileName = fileName;
+
+		try {
+			FileInputStream fileIn = new FileInputStream("/tmp/"+fileName+".ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			this = (Conversation) in.readObject();
+			in.close();
+			fileIn.close();
+		} catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+
 	}
 
-	public String getSenderAddress() {
+	public String[] getSenderAddress() {
 		return senderAddress;
 	}
-	public String getRecipientAddress() {
+	public String[] getRecipientAddress() {
 		return senderAddress;
 	}
-	public String getSenderName() {
+	public String[] getSenderName() {
 		return senderName;
 	}
-	public String getRecipientName() {
+	public String[] getRecipientName() {
 		return recipientName;
 	}
 
-	public void addEmail(String subject, String text) {
+	public void addEmail(String[] subject, String[] text) {
 		Email newEmail = new Email(senderAddress, recipientAddress, senderName, recipientName, subject, text);
 		emailCollection.add(newEmail);
+		try {
+			FireOutputStream fileOut = new FileOutputStream("tmp/"+fileName+".ser");
+			ObjectOutputStream out =new ObjectOutputStream(fileOut);
+			out.writeObject(this);
+			out.close();
+			fileOut.close();
+		} catch(IOException exception) {
+			exception.printStackTrace();
+		}
 	}
 }
