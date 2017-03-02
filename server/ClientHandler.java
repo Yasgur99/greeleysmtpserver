ae1a5ee157033b2f3ff691dcdf9e9ea54179dd96
yasgur99 <michaelmaitland@verizon.net>
2017-03-02 08:26:25 -0500

    cleaning up ClientHandler closing and begin central object

--- server/ClientHandler.java
+++ server/ClientHandler.java
@@ -1,4 +1,4 @@
-package server;
+package greeleysmtpserver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,58 +14,63 @@ import java.net.Socket;
  */
 public class ClientHandler implements Runnable {
 
-	private Socket connection;
-	private BufferedReader in;
-	private PrintWriter out;
+  private Socket connection;
+  private Boolean running;
+  private BufferedReader in;
+  private PrintWriter out;
 
-	public ClientHandler(Socket connection) {
-    	this.connection = connection;
-	}
+  public ClientHandler(Socket connection, Boolean running) {
+    this.connection = connection;
+    this.running = running;
+  }
 
-	@Override
-	public void run() {
-    	setupStreams();
-    	this.out.write("Connection with server has been made");
-    	readClientMessage();
-    	closeConnection();
-	}
+  @Override
+  public void run() {
+    setupStreams();
+    readClientMessage();
+    closeConnection();
+  }
 
-	private void setupStreams() {
-    	try {
-        	this.in = new BufferedReader(
-                	new InputStreamReader(
-                        	this.connection.getInputStream()));
-    	} catch (IOException ex) {
-        	System.out.println("IOException setting up input stream");
-    	}
+  private void setupStreams() {
+    try {
+      this.in = new BufferedReader(
+              new InputStreamReader(
+                      this.connection.getInputStream()));
+      System.out.println("Input stream setup");
+    } catch (IOException ex) {
+      System.out.println("IOException setting up input stream");
+    }
 
-    	try {
-        	this.out = new PrintWriter(
-                	new OutputStreamWriter(
-                        	this.connection.getOutputStream()));
-    	} catch (IOException ex) {
-        	System.out.println("IOException setting up output stream");
-    	}
-	}
+    try {
+      this.out = new PrintWriter(
+              new OutputStreamWriter(
+                      this.connection.getOutputStream()));
+          System.out.println("Output stream setup");
+          this.out.write("Connection with server has been made");
+    } catch (IOException ex) {
+      System.out.println("IOException setting up output stream");
+    }
+  }
 
-	private void readClientMessage() {
-    	try {
-        	String line = in.readLine();
-        	System.out.println("Client: " + line);
-    	} catch (IOException ex) {
-        	System.out.println("IOException reading the clients message");
-    	}
-	}
+  private void readClientMessage() {
+    try {
+      String line = in.readLine();
+      System.out.println("Client: " + line);
+      //TODO: send input to central object
+    } catch (IOException ex) {
+      System.out.println("IOException reading the clients message");
+    }
+  }
 
-	private void closeConnection() {
-    	try {
-        	this.in.close();
-        	this.out.write("Closing connection");
-        	this.out.close();
-        	this.connection.close();
-    	} catch (IOException ex) {
-        	System.out.println("Exception closing resources");
-    	}
-	}
-
+  private void closeConnection() {
+    try {
+      this.in.close();
+      this.out.write("Closing connection");
+      this.out.flush();
+      this.out.close();
+      this.connection.close();
+    } catch (IOException ex) {
+      System.out.println("IOException closing resources");
+    }
+  }
 }
