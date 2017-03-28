7402404942d77c0d05761d1fe1f025bb056a2979
yasgur99 <michaelmaitland@verizon.net>
2017-03-28 08:39:15 -0400

    totos in executor

--- actions/CommandExecutor.java
+++ actions/CommandExecutor.java
@@ -1,9 +1,10 @@
 package greeleysmtpserver;
+
 /**
  * @author michaelmaitland
  */
 
- import greeleysmtpserver.parser.SMTPCommand;
+import greeleysmtpserver.parser.SMTPCommand;
 
 public class CommandExecutor {
 
@@ -16,18 +17,25 @@ public class CommandExecutor {
     public String execute(){
         if(command.getCommandName().equals("HELO")){
             SMTPHelloCommand heloCommand = (SMTPHelloCommand)command;
+            //TODO: Verify Hostname
         }else if(command.getCommandName("MAIL FROM")){
             SMTPMailFromCommand mailFromCommand = (SMTPMailFromCommand)command;
+            //TODO: email sender, timestamp, relay server
         }else if(command.getCommandName("RCPT TO")){
             SMTPRcptCommand rcptToCommand = (SMTPRcptCommand)command;
+            //TODO: email reciever
         }else if(command.getCommandName("DATA")){
             SMTPDataCommand dataCommand = (SMTPDataCommand)command;
+            //TODO: Message Contents
         }else if(command.getCommandName("RSET")){
             SMTPResetCommand rsetCommand = (SMTPResetCommand)command;
+            //TODO: Clear state information so we dont store the same info (from above)
         }else if(command.getCommandName("VRFY")){
             SMTPVerifyCommand vrfyCommand = (SMTPVerifyCommand)command;
+            //TODO: return an error
         }else if(command.getCommandName("NOOP")){
             SMTPNoopCommand = noopCommand = (SMTPNoopCommand)command;
+            //WHY DOES THIS COMMAND EXIST
         }
     }
 }
